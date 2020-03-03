1a31d781032dd6f73ad943b4f851703f781e8fb0
Olalekan Babawale <padinality@yahoo.com>
2020-03-03 12:09:07 +0100

    finish setting up authentication

--- library/views.py
+++ library/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Book
 from .serializers import BookSerializer
 
 class BookView(viewsets.ModelViewSet):
+	search_fields = ['name']
+	filter_backends = (filters.SearchFilter,)
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
+
+# class BookBorrowView(viewsets.ModelViewSet):
+# 	queryset = Book.objects.all()
+# 	serializer_class = BookSerializer
